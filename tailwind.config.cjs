351b794e90236a719dde9782356485b8bb66ed8a
Charlie Say <charliewsay@gmail.com>
2022-12-29 23:51:21 +0000

    bumping marketing website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
   plugins: [require("@tailwindcss/typography")],
 };
